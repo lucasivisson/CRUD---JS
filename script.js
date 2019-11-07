9322bd4e624d3c5c58f446f6b0b54f6dc9bf9bfb
Lucas Ivisson <lucasivisson1@gmail.com>
2019-11-06 22:37:36 -0300

    Primeira atualização

---
+++ script.js
@@ -0,0 +1,14 @@
+function adicionar(){
+    var projeto= window.document.getElementById('projeto').value;
+    var stacks= window.document.getElementById('stacks').value;
+    var valor= window.document.getElementById('valor').value;
+    var dias= window.document.getElementById('dias').value;
+    var devs= window.document.getElementById('devs').value;
+
+    if(projeto.length == 0 || stacks.length == 0 || valor.length == 0 || dias.length == 0){
+        window.alert('Para cadastrar é necessário que todos os campos sejam preenchidos!')
+    }else{
+        
+    }
+
+}
