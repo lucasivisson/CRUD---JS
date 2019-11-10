0e99b084c2d5dcc68d5b27346ebe98d898ec6cd2
Lucas Ivisson <lucasivisson1@gmail.com>
2019-11-10 20:03:51 -0300

    att

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+var nome = window.document.getElementById('projeto');
+var stacks = window.document.getElementById('stacks');
+var valor = window.document.getElementById('valor');
+var dias = window.document.getElementById('dias');
+var devs = window.document.getElementById('devs');
+var infos = window.document.getElementById('infos');
+var resultfat = window.document.getElementsByClassName('resultfat')[0];
+var resultproj = window.document.getElementsByClassName('resultproj')[0];
+var resultporcent = window.document.getElementsByClassName('resultporcent');
+var result = 0;
 let projetos = [];
 function coletaInfo() {
     let nome = window.document.getElementById('projeto').value;
@@ -27,19 +37,19 @@ function verificar(projeto) {
 }
 function igual(target) {
     return false;
-//     if(projetos.length > 1) {
-//         projetos.filter(projeto => {
-//             if(projeto.name === target.name) {
-//                 return true;
-//             } else {
-//                 return false;
-//             }
-//         });
-//     }
+    //     if(projetos.length > 1) {
+    //         projetos.filter(projeto => {
+    //             if(projeto.name === target.name) {
+    //                 return true;
+    //             } else {
+    //                 return false;
+    //             }
+    //         });
+    //     }
 }
 function adicionar() {
     let projeto = coletaInfo();
-    if (verificar(projeto) || igual(projeto)){
+    if (verificar(projeto) || igual(projeto)) {
         window.alert('Para cadastrar é necessário que todos os campos sejam preenchidos!')
     } else {
         projetos.push(projeto);
@@ -62,7 +72,7 @@ function adicionar() {
         let cel4 = inserir.insertCell();
         let cel5 = inserir.insertCell();
         let cel6 = inserir.insertCell();
-        
+
         cel1.innerHTML = `${projeto.nome}`;
         cel2.innerHTML = `${projeto.stacks}`;
         cel3.innerHTML = `${projeto.valor}`;
@@ -70,8 +80,29 @@ function adicionar() {
         cel5.innerHTML = `${projeto.devs}`;
         cel6.appendChild(btn1);
         cel6.appendChild(btn2);
+
+        var resusoma = soma(valor.value);
+        var resuporcent = porcent(resusoma.value);
+        resultfat.innerHTML = `${resusoma}/27000`;
+        resultproj.innerHTML = `${projetos.length}/18`
+        resultprocent.innerHTML = `${resuporcent}`;
+
+        nome.value = '';
+        stacks.value = '';
+        valor.value = '';
+        dias.value = '';
+        devs.value = '';
+        nome.focus()
     }
 }
+function soma(n1) {
+    result += Number(n1);
+    return result;
+}
+function porcent(n){
+    result += (n*100)/27000;
+    return result;
+}
 function editar() {
-    
+
 }
