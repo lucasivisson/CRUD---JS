ef4d1a3d5d9fe196d9d420fab07f50e9a3c31313
Lucas Ivisson <lucasivisson1@gmail.com>
2019-11-14 23:19:07 -0300

    att

--- script.js
+++ script.js
@@ -4,15 +4,16 @@ var valor = window.document.getElementById('valor');
 var dias = window.document.getElementById('dias');
 var devs = window.document.getElementById('devs');
 var infos = window.document.getElementById('infos');
+var form = window.document.getElementById('tab1');
+var table = window.document.getElementById('infos')
 var resultfat = window.document.getElementsByClassName('resultfat')[0];
 var resultproj = window.document.getElementsByClassName('resultproj')[0];
 var resultporcent = window.document.getElementsByClassName('resultporcent')[0];
 var result = 0;
 var resu = 0;
-var insert = [];
-let projetos = [];
-var table = window.document.getElementById('infos');
+var re = 0;
 let selectRow = null;
+let selectv = null;
 function coletaInfo() {
     let nome = window.document.getElementById('projeto').value;
     let stacks = window.document.getElementById('stacks').value;
@@ -56,7 +57,6 @@ function adicionar() {
     if (verificar(projeto) || igual(projeto)) {
         window.alert('Para cadastrar é necessário que todos os campos sejam preenchidos!')
     } else {
-        projetos.push(projeto);
 
         let inserir = infos.insertRow();
         let cel1 = inserir.insertCell();
@@ -76,7 +76,7 @@ function adicionar() {
         btn1.setAttribute('id', 'botao1');
         btn1.setAttribute('type', 'button');
         btn1.setAttribute('value', 'editar');
-        btn1.setAttribute('onclick', 'editar()');
+        btn1.setAttribute('onclick', 'editar(this)');
         let btn2 = document.createElement('input');
         btn2.setAttribute('id', 'botao2');
         btn2.setAttribute('type', 'button');
@@ -85,12 +85,6 @@ function adicionar() {
         cel6.appendChild(btn1);
         cel6.appendChild(btn2);
 
-        var resusoma = soma(valor.value);
-        resultfat.innerHTML = `R$ ${resusoma.toFixed(1)}/27000`;
-        resultproj.innerHTML = `${projetos.length}/18`;
-        var resuporcent = porcent();
-        resultporcent.innerHTML = `${resuporcent.toFixed(2)}%`;
-        console.log(projetos);
         limpar();
     }
 }
@@ -102,21 +96,64 @@ function limpar() {
     devs.value = '';
     nome.focus()
 }
-function soma(n1) {
-    result += Number(n1);
-    return result;
-}
-function porcent() {
-    resu += result;
-    resu = (resu * 100) / 27000;
-    return resu;
-}
 function excluir(bot) {
     var confirm = window.confirm('Tem certeza que deseja excluir esse projeto?');
-    if(confirm){
+    if (confirm) {
         selectrow = bot.parentElement.parentElement;
-        let deletar = selectrow;
-        deletar.parentNode.removeChild(deletar);
+        selectrow.parentNode.removeChild(selectrow);
     }
 }
+function editar(bot) {
+    selectrow = bot.parentElement.parentElement;
+    selectcel1 = selectrow.cells[0].innerHTML;
+    selectcel2 = selectrow.cells[1].innerHTML;
+    selectcel3 = selectrow.cells[2].innerHTML;
+    selectcel4 = selectrow.cells[3].innerHTML;
+    selectcel5 = selectrow.cells[4].innerHTML;
+    nome.value = selectcel1;
+    stacks.value = selectcel2;
+    valor.value = selectcel3;
+    dias.value = selectcel4;
+    devs.value = selectcel5;
 
+    var botaoadd = window.document.getElementById('botao');
+    botaoadd.style.display = "none";
+
+    let btn3 = document.createElement('input');
+    btn3.setAttribute('id', 'botao3');
+    btn3.setAttribute('type', 'button');
+    btn3.setAttribute('value', 'Concluir');
+    btn3.setAttribute('onclick', 'editarmsm(selectrow)');
+    form.appendChild(btn3);
+    let btn4 = document.createElement('input');
+    btn4.setAttribute('id', 'botao4');
+    btn4.setAttribute('type', 'button');
+    btn4.setAttribute('value', 'Cancelar');
+    btn4.setAttribute('onclick', 'cancelar()');
+    form.appendChild(btn4);
+
+}
+function editarmsm(r) {
+    selectcel1 = r.cells[0];
+    selectcel2 = r.cells[1];
+    selectcel3 = r.cells[2];
+    selectcel4 = r.cells[3];
+    selectcel5 = r.cells[4];
+
+    selectcel1.innerHTML = `${nome.value}`;
+    selectcel2.innerHTML = `${stacks.value}`;
+    selectcel3.innerHTML = `${valor.value}`;
+    selectcel4.innerHTML = `${dias.value}`;
+    selectcel5.innerHTML = `${devs.value}`;
+
+    limpar();
+    botao3.style.display = "none";
+    botao4.style.display = "none";
+    botao.style.display = "block";
+}
+function cancelar(btn3, btn4) {
+    limpar();
+    botao3.style.display = "none";
+    botao4.style.display = "none";
+    botao.style.display = "block";
+}
